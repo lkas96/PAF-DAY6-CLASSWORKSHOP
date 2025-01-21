4d06fb6ae77d4683a8c8e9ca3b139dce3a94d1d2
lkas96 <kwek_lawson@hotmail.com>
2025-01-21 13:37:59 +0800

    Initial Template

---
+++ src/main/java/vttp/b5/paf/day6/classworkshop/Day6/Class/Workshop/Spotify/Songs/Day6ClassWorkshopSpotifySongsApplication.java
@@ -0,0 +1,13 @@
+package vttp.b5.paf.day6.classworkshop.Day6.Class.Workshop.Spotify.Songs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day6ClassWorkshopSpotifySongsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day6ClassWorkshopSpotifySongsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vttp/b5/paf/day6/classworkshop/Day6/Class/Workshop/Spotify/Songs/Day6ClassWorkshopSpotifySongsApplicationTests.java
@@ -0,0 +1,13 @@
+package vttp.b5.paf.day6.classworkshop.Day6.Class.Workshop.Spotify.Songs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day6ClassWorkshopSpotifySongsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
